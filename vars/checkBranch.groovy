a675dad45ca2c6703322462d67d1567bdc0c67bc
Kris Murray <109999760+kmurray2bee@users.noreply.github.com>
2023-10-12 18:12:05 -0700

    Update checkBranch.groovy

--- vars/checkBranch.groovy
+++ vars/checkBranch.groovy
@@ -1,9 +1,29 @@
 def call() {
-    if (env.BRANCH_IS_PRIMARY == 'true') {
-        echo "This is the primary branch."
-        // Perform actions specific to the primary branch
-    } else {
-        echo "This is not the primary branch."
-        // Perform actions for non-primary branches
+    pipeline {
+        agent any
+
+        options {
+            timestamps()
+            disableConcurrentBuilds()
+            skipDefaultCheckout(true)
+        }
+
+        stages {
+            stage('Run') {
+                steps {
+                    checkout scm
+                    script {
+                        // Access the BRANCH_IS_PRIMARY environment variable
+                        if (env.BRANCH_IS_PRIMARY == 'true') {
+                            echo "This is the primary branch."
+                            // Perform actions specific to the primary branch
+                        } else {
+                            echo "This is not the primary branch."
+                            // Perform actions for non-primary branches
+                        }
+                    }
+                }
+            }
+        }
     }
 }
